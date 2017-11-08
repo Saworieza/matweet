3b9703be8a5111562054f615b54656c0cc288a35
Sawo <saworieza@gmail.com>
2017-11-08 20:21:14 +0300

    remove traces of carrier shit

--- db/migrate/20171106050210_add_images_to_tweets.rb
+++
@@ -1,5 +0,0 @@
-class AddImagesToTweets < ActiveRecord::Migration[5.0]
-  def change
-    add_column :tweets, :images, :string, array: true, default: [].to_yaml # add images column as array
-  end
-end
